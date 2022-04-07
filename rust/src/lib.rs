f1687af8fdb26600ca1f116f6f78f0147d79344a
Matthew Lance Fuller <matthewlancefuller@gmail.com>
2022-04-06 22:22:47 -0700

    Playing with colors

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -56,3 +56,28 @@ pub fn start() {
 
     context.stroke();
 }
+
+#[wasm_bindgen]
+pub struct Client {
+
+}
+
+#[wasm_bindgen]
+impl Client {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        log("New was hit");
+        Self {
+
+        }
+    }
+
+    pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
+        log("Update was hit");
+        Ok(())
+    }
+
+    pub fn render(&self) {
+
+    }
+}
